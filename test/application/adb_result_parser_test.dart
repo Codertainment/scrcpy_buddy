e88af152586b06c03e4c61960fedd66d8a1c2671
Shripal Jain <shripal17@gmail.com>
2025-06-29 13:29:09 +0200

    Parameterize parseInitResult test
    Signed-off-by: Shripal Jain <shripal17@gmail.com>

--- test/application/adb_result_parser_test.dart
+++ test/application/adb_result_parser_test.dart
@@ -8,43 +8,56 @@ import 'package:scrcpy_buddy/application/model/adb/adb_device.dart';
 import 'package:scrcpy_buddy/application/model/adb/adb_error.dart';
 import 'package:test/test.dart';
 
+import '../description_parameterized_test.dart';
 import '../fixtures/process_exception_fixture.dart';
 import '../fixtures/process_result_fixture.dart';
+
+typedef _ProcessResultSupplier = Future<ProcessResult> Function();
 
 void main() {
   final AdbResultParser parser = AdbResultParser();
 
-  group('parseInitResult', () {
-    test('Success', () async {
-      final processResult = ProcessResultFixture.create(exitCode: 0);
-      final result = await parser.parseInitResult(Future.value(processResult));
-      expect(result.isRight(), true);
-      expect(result.getRight().getOrElse(() => throw "Unknown Error"), processResult.exitCode);
-    });
-
-    test('Failed to find executable', () async {
-      final processException = ProcessExceptionFixture.create(message: "failed to find");
-      final result = await parser.parseInitResult(Future.error(processException));
-      expect(result.isLeft(), true);
-      final resultError = result.getLeft().getOrElse(() => throw "Unknown error");
-      expect(resultError.runtimeType, AdbNotFoundError);
-    });
-
-    test('Unknown Process Exception', () async {
-      final unknownProcessException = ProcessExceptionFixture.create();
-      final result = await parser.parseInitResult(Future.error(unknownProcessException));
-      final resultError = result.getLeft().getOrElse(() => throw "Unknown error");
-      expect(resultError.runtimeType, UnknownAdbError);
-    });
-
-    test('Unknown Exception', () async {
-      final ioException = SignalException("");
-      final result = await parser.parseInitResult(Future.error(ioException));
-      expect(result.isLeft(), true);
-      final resultError = result.getLeft().getOrElse(() => throw "Unknown error");
-      expect(resultError.runtimeType, UnknownAdbError);
-    });
-  });
+  descriptionParameterizedTest(
+    'parseInitResult',
+    [
+      [
+        "Success",
+        () => Future<ProcessResult>.value(ProcessResultFixture.create(exitCode: 0)),
+        Either<AdbError, int>.right(0),
+        null,
+      ],
+      [
+        'Failed to find executable',
+        () => Future<ProcessResult>.error(ProcessExceptionFixture.create(message: "failed to find")),
+        Either<AdbError, int>.left(AdbNotFoundError()),
+        AdbNotFoundError,
+      ],
+      [
+        'Unknown Process Exception',
+        () => Future<ProcessResult>.error(ProcessExceptionFixture.create()),
+        Either<AdbError, int>.left(UnknownAdbError()),
+        UnknownAdbError,
+      ],
+      [
+        'Unknown Exception',
+        () => Future<ProcessResult>.error(SignalException("")),
+        Either<AdbError, int>.left(UnknownAdbError()),
+        UnknownAdbError,
+      ],
+    ],
+    (_, _ProcessResultSupplier processResultSupplier, Either<AdbError, int> expectedResult, expectedErrorType) async {
+      final result = await parser.parseInitResult(processResultSupplier());
+      expect(result.isRight(), expectedResult.isRight());
+      if (expectedResult.isRight()) {
+        expect(
+          result.getRight().getOrElse(() => throw "Unknown Error"),
+          expectedResult.getRight().getOrElse(() => throw "Unknown Error"),
+        );
+      } else {
+        expect(result.getLeft().getOrElse(() => throw "Unknown error").runtimeType, expectedErrorType);
+      }
+    },
+  );
 
   group('parseDevicesResult', () {
     test('Success', () async {
