3b1f5bcb3fa6c6afe7dfd29f55fccc89f584fd1d
Shripal Jain <shripal17@gmail.com>
2025-11-14 13:04:14 +0530

    Refactor settings screen structure - Move scrcpyExecutable section to separate widget
    Signed-off-by: Shripal Jain <shripal17@gmail.com>

---
+++ lib/presentation/settings/widget/scrcpy_executable_setting.dart
@@ -0,0 +1,122 @@
+import 'dart:io';
+
+import 'package:file_picker/file_picker.dart';
+import 'package:fluent_ui/fluent_ui.dart';
+import 'package:provider/provider.dart';
+import 'package:scrcpy_buddy/application/app_settings.dart';
+import 'package:scrcpy_buddy/application/extension/scrcpy_error_extension.dart';
+import 'package:scrcpy_buddy/presentation/extension/translation_extension.dart';
+import 'package:scrcpy_buddy/presentation/widgets/app_widgets.dart';
+import 'package:scrcpy_buddy/service/scrcpy_service.dart';
+
+class ScrcpyExecutableSetting extends StatefulWidget {
+  const ScrcpyExecutableSetting({super.key});
+
+  @override
+  State<ScrcpyExecutableSetting> createState() => _ScrcpyExecutableSettingState();
+}
+
+class _ScrcpyExecutableSettingState extends AppModuleState<ScrcpyExecutableSetting> {
+  @override
+  String get module => 'settings.scrcpyExecutable';
+
+  late final _settings = context.read<AppSettings>();
+  late final _scrcpyService = context.read<ScrcpyService>();
+
+  final _infoFlyoutController = FlyoutController();
+  final _scrcpyExecutableController = TextEditingController();
+  bool _isSavingScrcpyExecutable = false;
+  bool _isCheckingVersionInfo = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _scrcpyExecutableController.text = _settings.scrcpyExecutable.getValue();
+  }
+
+  Future<void> _validateAndSave(String? path) async {
+    if (path == null) return;
+    setState(() => _isSavingScrcpyExecutable = true);
+    final file = File(path);
+    if (await file.exists()) {
+      await _settings.scrcpyExecutable.setValue(path);
+    } else {
+      showInfoBar(
+        title: translatedText(key: 'invalidPath'),
+        severity: InfoBarSeverity.warning,
+      );
+    }
+    setState(() => _isSavingScrcpyExecutable = false);
+  }
+
+  Future<void> _checkVersionInfo() async {
+    setState(() => _isCheckingVersionInfo = true);
+    final result = await _scrcpyService.getVersionInfo(_scrcpyExecutableController.text);
+    result.mapLeft(
+      (left) => showInfoBar(
+        title: context.translatedText(key: 'common.somethingWentWrong'),
+        content: left.message,
+        severity: InfoBarSeverity.error,
+      ),
+    );
+    result.map((info) {
+      _infoFlyoutController.showFlyout(
+        builder: (context) {
+          return FlyoutContent(
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                WindowsIcon(FluentIcons.skype_circle_check, color: Colors.successPrimaryColor),
+                const SizedBox(width: 8),
+                Flexible(child: Text(info)),
+              ],
+            ),
+          );
+        },
+      );
+    });
+    setState(() => _isCheckingVersionInfo = false);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+          child: TextBox(
+            controller: _scrcpyExecutableController,
+            enabled: !_isSavingScrcpyExecutable,
+            textInputAction: TextInputAction.done,
+            onSubmitted: (value) => _validateAndSave(value),
+            onTapOutside: (_) => _validateAndSave(_scrcpyExecutableController.text),
+            suffix: _isSavingScrcpyExecutable
+                ? ProgressRing()
+                : IconButton(
+                    icon: Icon(WindowsIcons.file_explorer),
+                    onPressed: () async {
+                      final result = await FilePicker.platform.pickFiles(
+                        dialogTitle: translatedText(key: 'dialogTitle'),
+                      );
+
+                      if (result != null) {
+                        _validateAndSave(result.files.single.path);
+                      }
+                    },
+                  ),
+          ),
+        ),
+        const SizedBox(width: 16),
+        FlyoutTarget(
+          controller: _infoFlyoutController,
+          child: _isCheckingVersionInfo
+              ? ProgressRing()
+              : Button(
+                  onPressed: _checkVersionInfo,
+                  child: Text(translatedText(key: 'check')),
+                ),
+        ),
+      ],
+    );
+  }
+}
