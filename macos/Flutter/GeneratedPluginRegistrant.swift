2ab5b5edfcad79c4923d25e1b6e52ea9f631ab24
Shripal Jain <shripal17@gmail.com>
2025-11-26 22:32:03 +0530

    Upgrade objectBox to latest version - Older version was failing because of short-hand operator - Use forked reflectable_builder to be compatible with newer dependencies required by ObjectBox upgrade (temporary until fixed by `reflectable_builder`)
    Signed-off-by: Shripal Jain <shripal17@gmail.com>

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,6 +8,7 @@ import Foundation
 import file_picker
 import macos_window_utils
 import objectbox_flutter_libs
+import path_provider_foundation
 import screen_retriever_macos
 import shared_preferences_foundation
 import system_theme
@@ -17,6 +18,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
   MacOSWindowUtilsPlugin.register(with: registry.registrar(forPlugin: "MacOSWindowUtilsPlugin"))
   ObjectboxFlutterLibsPlugin.register(with: registry.registrar(forPlugin: "ObjectboxFlutterLibsPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   ScreenRetrieverMacosPlugin.register(with: registry.registrar(forPlugin: "ScreenRetrieverMacosPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SystemThemePlugin.register(with: registry.registrar(forPlugin: "SystemThemePlugin"))
