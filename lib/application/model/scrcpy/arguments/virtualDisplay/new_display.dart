fe941ae009e3a704dfb43e297baecbe85d6e7148
Shripal Jain <shripal17@gmail.com>
2025-11-29 14:40:31 +0530

    Introduce newDisplay CliArgument for virtualDisplay section
    Signed-off-by: Shripal Jain <shripal17@gmail.com>

---
+++ lib/application/model/scrcpy/arguments/virtualDisplay/new_display.dart
@@ -0,0 +1,17 @@
+import 'package:scrcpy_buddy/application/model/scrcpy/scrcpy_cli_argument.dart';
+import 'package:scrcpy_buddy/main.dart';
+
+@scrcpyArg
+class NewDisplay extends ScrcpyCliArgument<String> {
+  @override
+  List<String> toArgs(String value) => [argument, value];
+
+  @override
+  final String argument = '--new-display';
+
+  @override
+  final String label = 'virtualDisplay.newDisplay';
+
+  @override
+  final List<String>? values = null;
+}
