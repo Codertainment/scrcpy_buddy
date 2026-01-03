3357dbbd18606240fce31ae50aea4c95581e4fd9
Shripal Jain <shripal17@gmail.com>
2026-01-03 15:09:40 +0100

    Add preference for adb executable in app settings

--- lib/application/app_settings.dart
+++ lib/application/app_settings.dart
@@ -4,17 +4,24 @@ import 'package:streaming_shared_preferences/streaming_shared_preferences.dart';
 
 class AppSettings {
   static const _PREFIX = "settings";
+  static const _KEY_ADB_EXECUTABLE = "adbExecutable";
   static const _KEY_SCRCPY_EXECUTABLE = "scrcpyExecutable";
   static const _KEY_THEME_BRIGHTNESS = "theme.brightness";
   static const _KEY_IS_LAST_USED_PROFILE_DEFAULT = "isLastUsedProfileDefault";
   static const _KEY_DEFAULT_PROFILE_ID = "defaultProfileId";
 
   AppSettings(SharedPrefs prefs)
-    : scrcpyExecutable = prefs.getString(prefix: _PREFIX, key: _KEY_SCRCPY_EXECUTABLE),
+    : adbExecutable = prefs.getString(prefix: _PREFIX, key: _KEY_ADB_EXECUTABLE),
+      scrcpyExecutable = prefs.getString(prefix: _PREFIX, key: _KEY_SCRCPY_EXECUTABLE),
       themeBrightness = prefs.getBrightness(prefix: _PREFIX, key: _KEY_THEME_BRIGHTNESS),
-      isLastUsedProfileDefault = prefs.getBool(prefix: _PREFIX, key: _KEY_IS_LAST_USED_PROFILE_DEFAULT, defaultValue: true),
+      isLastUsedProfileDefault = prefs.getBool(
+        prefix: _PREFIX,
+        key: _KEY_IS_LAST_USED_PROFILE_DEFAULT,
+        defaultValue: true,
+      ),
       defaultProfileId = prefs.getInt(prefix: _PREFIX, key: _KEY_DEFAULT_PROFILE_ID);
 
+  final Preference<String> adbExecutable;
   final Preference<String> scrcpyExecutable;
   final Preference<Brightness?> themeBrightness;
   final Preference<bool> isLastUsedProfileDefault;
